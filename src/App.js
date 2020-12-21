2e9cc5391c466398288ade90af7cda9f64bb278b
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-12-21 22:59:27 +0100

    Get list of countries form API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("");
+  console.log(countries);
+
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setCountries(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+
   return (
-    <div >
-      <h1>Hello Countries!!</h1>
+    <div>
+      <form action="">
+        <label htmlFor="">
+          Find country
+          <input
+            type="text"
+            placeholder="find country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </label>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
