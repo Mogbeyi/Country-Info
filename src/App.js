fe1479e5dfe38e63ce0f5bfe27760275e7ec61fd
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-12-22 15:00:40 +0100

    Display languages and flags

--- src/App.js
+++ src/App.js
@@ -39,6 +39,29 @@ const App = () => {
         <p>Too many matches, specify another title</p>
       </div>
     );
+  } else if (countryLength === 1) {
+    const nation = filterCountries[0];
+
+    return (
+      <div>
+        <Form handleChange={handleChange} country={country} />
+
+        <h1>{nation.name}</h1>
+        <p>Capital: {nation.capital}</p>
+        <p>Population: {nation.population}</p>
+
+        <strong>Languages</strong>
+
+        <ul>
+          {nation.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+
+        <img src={nation.flag} alt="" width="150" height="100"/>
+
+      </div>
+    );
   } else if (countryLength <= 10) {
     return (
       <div>
